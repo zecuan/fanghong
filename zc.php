b00655b4a548e45a6b69a93fd3432b81fdf9c4be
zecuan <3154381043@qq.com>
2025-11-07 15:25:07 +0800

    Add initial HTML structure for GitHub Pages

---
+++ zc.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta charset="UTF-8">
+  <title>欢迎访问我的 GitHub 页面</title>
+</head>
+<body style="font-family: sans-serif; text-align:center; margin-top:100px;">
+  <h1>Hello from GitHub Pages 👋</h1>
+  <p>这是我的第一个静态网站！</p>
+</body>
+</html>
